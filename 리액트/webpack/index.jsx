7fdacd7394fbbf9bb066f7d3a96ca88f17233c9a
test <test@test.com>
2021-07-05 18:13:01 +0900

    test

--- 리액트/webpack/index.jsx
+++ 리액트/webpack/index.jsx
@@ -6,7 +6,7 @@ const ReactDOM = require('react-dom')
 class App extends React.Component{
     render(){
         return(
-            <div>Hello WebPack!!!!</div>
+            <div>Hello WebPack!11!!!</div>
         )
     }
 }
